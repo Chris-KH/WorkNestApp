ddbf000a8c5519fcbc621a059ef5dd55266c7d70
Chris-KH <klowgamervn@gmail.com>
2025-07-09 13:44:06 +0700

    add theme color

--- app/src/main/java/com/apcs/worknestapp/ui/theme/Theme.kt
+++ app/src/main/java/com/apcs/worknestapp/ui/theme/Theme.kt
@@ -8,28 +8,39 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    background = Color(0xFF181818), //OK
+    onBackground = Color(0xFFFFFFFF), //OK
+    surface = Color(0xFF303030), // OK
+    onSurface = Color(0xFFFFFFFF), //OK
+    primary = Purple80, //TODO
+    onPrimary = Purple80, //TODO
+    secondary = Color(0xFF2E7D32), //OK
+    onSecondary = Color(0xFFDFFFE2), //OK
+    tertiary = Color(0xFFF9A825), //OK
+    onTertiary = Color(0xFFFFF8E1), // OK
+    error = Color(0xFFCF6679), //OK
+    onError = Color(0xFFFFFFFF), //OK
+    outline = Color(0xFFA6988A),
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    background = Color(0xFFFFFFFF), // OK
+    onBackground = Color(0xFF000000), //TODO
+    surface = Color(0xFFEEEEEE), //OK
+    onSurface = Color(0xFF000000), //TODO
+    primary = Purple40, //TODO
+    onPrimary = Purple80, //TODO
+    secondary = Color(0xFF1B3B1B), //Ok
+    onSecondary = Color(0xFFDFFFE2), //OK
+    tertiary = Color(0xFFE3960E), //OK
+    onTertiary = Color(0xFFFFF8E1), //OK
+    error = Color(0xFFB00020), //OK
+    onError = Color(0xFFFFFFFF), //OK
+    outline = Color(0xFFD9D0C2),
 )
 
 @Composable
