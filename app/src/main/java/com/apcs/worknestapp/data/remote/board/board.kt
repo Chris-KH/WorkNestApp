3705fe40dbda072651723f7358a25d5e106022d9
NghiaLe4575 <160830150+NghiaLe4575@users.noreply.github.com>
2025-08-25 11:00:37 +0700

    Board dataclass

---
+++ app/src/main/java/com/apcs/worknestapp/data/remote/board/board.kt
@@ -0,0 +1,24 @@
+package com.apcs.worknestapp.data.remote.board
+
+import com.apcs.worknestapp.data.remote.note.Note
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.DocumentId
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.ServerTimestamp
+
+data class Board(
+    @DocumentId val docId: String? = null,
+    val cover: Int? = null,
+    val name: String? = null,
+    val notelists: List<Notelist> = emptyList(),
+    val isLoading: Boolean? = null,
+    val members: List<DocumentReference?> = emptyList() /// member list.
+)
+data class Notelist(
+    @DocumentId val docId: String? = null,
+    val name: String? = null,
+    val cover: Int? = null,
+    val archived: Boolean? = null,
+    @ServerTimestamp val createdAt: Timestamp? = null,
+    val notes: List<Note> = emptyList()
+)
---
+++ app/src/main/java/com/apcs/worknestapp/ui/screens/board/BoardScreen.kt
@@ -0,0 +1,18 @@
+package com.apcs.worknestapp.ui.screens.board
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NoteScreen(
+    navController: NavHostController,
+    snackbarHost: SnackbarHostState,
+    modifier: Modifier = Modifier
+)
+{
+
+}
